319d955eec94c14a5f5c0686a4e596e94fe273d3
Chia-Hung Lin <clin4j@googlemail.com>
2021-03-07 22:08:33 +0800

    Add zio library

--- src/main/scala/bully/Node.scala
+++ src/main/scala/bully/Node.scala
@@ -1,11 +1,36 @@
 package bully
 
 import scala.util.Random
+import zio._
 
 sealed trait Message
 case object Halt extends Message
 
 object Node {
+
+  trait NodeP {
+    def receive(m: Message): IO[Exception, Unit]
+
+    def create[I, O](id: Int = getId) = {
+      def process(m: Message) =
+        for {
+          _ <- receive(m)
+        } yield ()
+      for {
+        queue <- Queue.bounded[Message](requestedCapacity = 32)
+        _ <- (for {
+            m <- queue.take
+            _ <- process(m)
+          } yield ()).forever.fork
+      } yield new Node(id = Option(id), queue = queue)
+    }
+  }
+
+  def defaultOps[I, O] =
+    (in: I) => {
+      println("Default operation ...")
+      ().asInstanceOf[O]
+    }
 
   /**
     * Id value is between 1 and N
@@ -13,11 +38,18 @@ object Node {
     */
   def getId: Int = Random.nextInt() & Int.MaxValue
 
-  def create[I, O](id: Int = getId): Node[I, O] = Node[I, O](id = Option(id))
 }
 case class Node[I, O](
     id: Option[Int] = None,
     role: Option[Role] = None,
     status: Status = Down,
-    f: I => O = (in: I) => { println("start function ..."); ().asInstanceOf[O] }
-)
+    queue: Queue[Message],
+    f: I => O = Node.defaultOps[I, O]
+) {
+
+  def !(message: Message): UIO[Message] =
+    for {
+      _ <- queue.offer(message)
+    } yield message
+
+}
